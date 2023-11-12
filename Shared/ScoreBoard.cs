b92b6cb2e0758651c30af220df9f355fd3d1a9c0
alexgoold <alexander.goold@iths.se>
2023-11-12 21:29:43 +0100

    Add project files.

---
+++ Shared/ScoreBoard.cs
@@ -0,0 +1,65 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace TennisGame.Shared;
+
+public class ScoreBoard
+{
+	public TennisPlayer Player1 { get; set; }
+	public TennisPlayer Player2 { get; set; }
+	public string ScoreAsString { get; set; }
+
+	public ScoreBoard(TennisPlayer player1, TennisPlayer player2)
+	{
+		Player1 = player1;
+		Player2 = player2;
+		player1.Name = "Player 1";
+		player2.Name = "Player 2";
+		ScoreAsString = $"{Player1.Score} - {Player2.Score}";
+	}
+	public void AddPointToPlayer(TennisPlayer player)
+	{
+		player.Points++;
+		player.Score = player.ConvertNumberToTennisScore(player.Points);
+		ScoreAsString = Score();
+	}
+
+	public string Score()
+	{
+		if (Player1.Points == Player2.Points)
+		{
+			Player1.Score = Player2.Score =
+				Player1.Points < 3
+				? Player1.ConvertNumberToTennisScore(Player1.Points)
+				: "Deuce";
+		}
+		else if (Player1.Points >= 4 && Player1.Points >= Player2.Points + 2)
+		{
+			return $"{Player1.Name} wins";
+		}
+		else if (Player2.Points >= 4 && Player2.Points >= Player1.Points + 2)
+		{
+			return $"{Player2.Name} wins";
+		}
+		else if (Player1.Points >= 4 && Player1.Points == Player2.Points + 1)
+		{
+			return $"{Player1.Name} advantage";
+		}
+		else if (Player2.Points >= 4 && Player2.Points == Player1.Points + 1)
+		{
+			return $"{Player2.Name} advantage";
+		}
+
+		return $"{Player1.Score} - {Player2.Score}";
+
+	}
+
+	public void ResetRound()
+	{
+		Player1.Points = 0;
+		Player2.Points = 0;
+		Player1.Score = "Love";
+		Player2.Score = "Love";
+		ScoreAsString = $"{Player1.Score} - {Player2.Score}";
+	}
+
+}
---
+++ Shared/TennisPlayer.cs
@@ -0,0 +1,20 @@
+﻿namespace TennisGame.Shared;
+
+public class TennisPlayer
+{
+	public string Name { get; set; } = string.Empty;
+	public string Score { get; set; } = "Love";
+	public int Points { get; set; } = 0;
+
+	public string ConvertNumberToTennisScore(int score)
+	{
+		return score switch
+		{
+			0 => "Love",
+			1 => "15",
+			2 => "30",
+			3 => "40",
+			_ => "You should never see this",
+		};
+	}
+}
